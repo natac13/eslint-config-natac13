1b2f7dd606202bbaa482dfe3754c2fee6d87ea4a
natac13 <sean.campbell13@gmail.com>
2016-04-04 17:14:08 -0400

    added config to index.js

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ module.exports = {
       'capIsNewExceptions': ['Map', 'Set', 'List', 'Promise'],
     }],
     'max-len': ['error', { 'code': 120, 'tabWidth': 2, 'ignoreComments': true }],
-    'quote-props': ['error', 'consistent'],
+    'quote-props': ['error', 'always'],
+  },
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
+  'parserOptions': {
+    'ecmaFeatures': {
+      'ecmaVersion': 6,
+      'experimentalObjectRestSpread': true,
+      'jsx': true,
+    },
+    'sourceType': 'module',
   },
 };
