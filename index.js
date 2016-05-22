ec13bb914cfcbe2152ef1ba5af4eb267d0bac6c7
natac13 <sean.campbell13@gmail.com>
2016-05-22 08:33:18 -0400

    arrow-callback

--- index.js
+++ index.js
@@ -6,15 +6,17 @@ module.exports = {
   'parser': 'babel-eslint',
   'rules': {
     'new-cap': ['error', {
-      'capIsNewExceptions': ['Map', 'Set', 'List', 'Promise'],
+      'capIsNewExceptions': ['Map', 'Set', 'List', 'Promise', 'express.Router'],
     }],
     'max-len': ['error', { 'code': 120, 'tabWidth': 2, 'ignoreComments': true }],
     'quote-props': ['error', 'consistent-as-needed'],
+    "prefer-arrow-callback": ["error", { "allowNamedFunctions": true }],
   },
   'env': {
     'browser': true,
     'es6': true,
     'node': true,
+    'mocha': true,
   },
   'parserOptions': {
     'ecmaFeatures': {
